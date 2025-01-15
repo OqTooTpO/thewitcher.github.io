64874244b6b94119ad967eb66d2011439bebb68e
Proxima <nanamofeevn@gmail.com>
2025-01-15 21:19:22 +0300

    thewitcher

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper(".slider", {
+  direction: "vertical",
+  speed: 2400,
+  mausewheel: {
+    enabled: true,
+    sensitivity: 2.4,
+  },
+  spaceBetween: 18,
+  parallax: true,
+  freeMode: true,
+});
